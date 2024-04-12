996b503ddd64ccc75683adcdfb3b492d2b12c776
皮昌钱 <2907678297@qq.com>
2024-04-12 16:29:46 +0800

    add readme

---
+++ readme.md
@@ -0,0 +1,7 @@
+# ZSH 配置
+
+```shell
+sudo apt install zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" # oh my zsh
+sh -c "$(wget https://raw.githubusercontent.com/Rookiecom/dotfile/main/zsh/auto_configuration.sh -O -)"
+```
--- zsh/auto_configuration.sh
+++ zsh/auto_configuration.sh
@@ -1,32 +1,33 @@
-sudo apt install zsh
-sudo chsh -s /bin/zsh
-
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" # oh my zsh
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install # fzf install, need key to configure
 
-git clone https://github.com/Aloxaf/fzf-tab $ZSH_CUSTOM/plugins/fzf-tab
+git clone --depth=1 https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fzf-tab
 # fzf-tab
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 # p10k
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 # zsh-syntax-highlighting
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # zsh-autosuggestions
 
-git clone https://github.com/Rookiecom/dotfile.git
+git clone --depth=1 https://github.com/Rookiecom/dotfile.git
 
 cp dotfile/zsh/.zshrc ~/
 # 最重要的zsh 配置文件
 
+cp dotfile/zsh/.p10k.zsh ~/
+
 cp -r dotfile/zsh/.zfunc ~/
 # 一些命令的补全规则
 
-cp dotfile/zsh/{.fzf-tab.zsh,.fzf.preview.content} ~/
+cp dotfile/zsh/.fzf-tab.zsh ~/
+cp dotfile/zsh/.fzf.preview.content ~/
 # fzf-tab configuration
 
-cp dotfile/zsh/lesspipe.sh /usr/local/bin/
+sudo cp dotfile/zsh/lesspipe.sh /usr/local/bin/
 # fzf tab preview 备用方案
+
+rm -rf dotfile
